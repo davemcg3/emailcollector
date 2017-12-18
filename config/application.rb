1867a3b2e3f8ee819e5bf224ce90bb4ae366681c
davemcg3 <dave.mcgrath3@gmail.com>
2017-12-18 02:12:59 -0800

    Update to serve static assets, but this should be taken care of by the heroku gem and environment variable

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Emailcollector
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # For serving assets in production, this should probably go in environments/production.rb but was recommended to put here on SO
+    config.serve_static_files = true
   end
 end
