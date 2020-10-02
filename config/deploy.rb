4db9fead2f968b2ccd2cade9c0e644ff56fa263c
davemcg3 <dave.mcgrath3@gmail.com>
2020-10-02 00:09:43 -0700

    Fixing capistrano deploys and commented out invalid option in puma file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-server 'emailcollector', port: 30001, roles: [:web, :app, :db], primary: true
+# server 'ctsv', port: 30001, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:davemcg3/emailcollector.git'
 set :application,     'emailcollector'
@@ -17,7 +17,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa-github-davemcg3.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false  # Change to true if using ActiveRecord
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,6 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+server "172.251.188.199", user: "dave", roles: %w{app db web}
 
 
 
@@ -20,6 +21,9 @@
 # role :app, %w{deploy@example.com}, my_property: :my_value
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
+# role :app, %w{dave@ctsv}
+# role :web, %w{dave@ctsv}
+# role :db,  %w{dave@ctsv}
 
 
 
@@ -42,7 +46,7 @@
 # Global options
 # --------------
 #  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
+#    keys: %w(/home/user_name/.ssh/id_rsa-github-davemcg3),
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
