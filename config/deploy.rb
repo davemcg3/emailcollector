dfe3e494b8fb2a90e1838639a5240836d72be226
davemcg3 <dave.mcgrath3@gmail.com>
2020-10-07 01:43:20 -0700

    Adjust path to sock and pid files

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,9 +12,9 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/var/www/apps/ruby/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind,       "unix://#{shared_path}/puma.sock"
+set :puma_state,      "#{shared_path}/puma.state"
+set :puma_pid,        "#{shared_path}/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa-github-davemcg3.pub) }
