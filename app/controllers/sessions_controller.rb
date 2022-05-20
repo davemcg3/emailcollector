dcf515339c10d5cd63654ad64dfca24b2af03167
davemcg3 <dave.mcgrath3@gmail.com>
2022-05-19 23:29:36 -0600

    Added more logging to troubleshoot

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    Rails.logger.debug params[:email]
     user = User.find_by_email(params[:email])
+    Rails.logger.debug user
+    Rails.logger.debug user.authenticate(params[:password])
     # If the user exists AND the password entered is correct.
     if user && user.authenticate(params[:password])
-      logger.info("Logged in as #{user.name}")
+      Rails.logger.debug("Logged in as #{user.name}")
 
-      # Save the user id inside the browser cookie. This is how we keep the user 
+      # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
       flash[:success] = "Logged in as #{user.name}"
       redirect_to '/'
     else
-      logger.info("Failed to login as #{params[:email]}")
+      Rails.logger.debug("Failed to login as #{params[:email]}")
       # If user's login doesn't work, send them back to the login form.
       flash[:danger] = "Unable to login"
       redirect_to '/login'
@@ -23,7 +26,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    logger.info("Logged out")
+    Rails.logger.debug("Logged out")
     #session[:user_id] = nil
     session.delete(:user_id)
     flash[:warning] = 'Logged out'
