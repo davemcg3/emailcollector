27875cb053b55e42093aed2aaa9ab5f10cc6624f
davemcg3 <dave.mcgrath3@gmail.com>
2022-05-19 23:21:43 -0600

    Added debugging statements to figure out what's wrong on production when logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,25 @@ class ApplicationController < ActionController::Base
   @debug = nil
 
   def current_user
+    Rails.logger.debug "3"
+    Rails.logger.debug @current_user
+    Rails.logger.debug session[:user_id]
+    Rails.logger.debug User.find(session[:user_id]) if session[:user_id]
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
 
   def authorize
+    Rails.logger.debug "authorizing"
     redirect_to '/' unless current_user
   end
 
   def is_admin
+    Rails.logger.debug "is_admin"
+    Rails.logger.debug current_user
+    Rails.logger.debug current_user.admin
   	if not current_user.admin
+      Rails.logger.debug "Unauthorized redirecting to /"
   		flash[:danger] = 'Unauthorized'
   		redirect_to '/'
   	end
